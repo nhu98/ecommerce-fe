7b5e20d04aefa96e05639d738dfd9855c2f7c36c
nhuhq <nhuhq@imt-soft.com>
2025-02-11 17:17:15 +0700

    Feat: init project!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}",],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        // 👇 Add CSS variables
+        mono: ["var(--font-roboto-mono)"],
+      }, colors: {
+        background: "var(--background)", foreground: "var(--foreground)",
       },
     },
   },
